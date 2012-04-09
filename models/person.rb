5586ca6eec336063bcd26ef4bf44a5e1ea4b8008
Adam Scott <adam.d.scott@gmail.com>
2012-04-09 16:49:32 +0800

    Working examples

---
+++ models/person.rb
@@ -0,0 +1,14 @@
+require 'active_model'
+
+class Person
+  include ActiveModel::Validations
+
+  validates_length_of :name, :maximum => 40, :message => 'name too long'
+
+  attr_reader :name
+
+  def initialize(attrs)
+    @name = attrs[:name]
+  end
+
+end
---
+++ spec/person_spec.rb
@@ -0,0 +1,21 @@
+require 'rspec'
+require File.expand_path('models/person.rb')
+
+describe Person do
+
+  describe 'validation' do
+
+    it 'should be valid' do
+      person = Person.new(:name => 'Bruce Wayne')
+      person.should be_valid
+    end
+
+    it 'should be invalid if name is too long' do
+      person = Person.new(:name => 'a' * 41)
+      person.should_not be_valid
+      person.errors[:name].should == ['name too long']
+    end
+
+  end
+
+end
